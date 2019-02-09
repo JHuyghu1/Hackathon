8f1b91e77b432cb7dbd02b915db4e5c19cb80436
Dyandra Allen <dallen9@binghamton.edu>
2019-02-09 17:33:41 -0500

    Rules files

--- Lsystem.py
+++ Lsystem.py
@@ -1,6 +1,24 @@
 class Lsystem:
     def __init__(self,filename):
-        pass
+        try:
+            self.lsys = open(filename, "r")
+        except FileNotFoundError:
+            print("File Doesn't Exist")
+        else:
+            self.angle = int(self.lsys.readline())
+            self.iteration = int(self.lsys.readline())
+            self.distance = int(self.lsys.readline())
+            self.axiom = self.lsys.readline()
+            self.rules = {}
+            self.state = []
+            while True:
+                rul = self.lsys.readline()
+                if not rul:
+                    break
+                symbol = rul[0]
+                sub = rul[4:(len(rul)-1)]
+                self.rules[symbol] = sub
+            self.lsys.close()
 
     def createLsystem(self):
         pass
--- color.py
+++ color.py
@@ -1,4 +1,10 @@
 import turtle
+import random
+import lsystem
+
+def getRules():
+    fn = "Rules" + str(random.randint(1,2)) + ".txt"
+    return fn
 
 def colorTurtle():
     wn = turtle.Screen()
@@ -11,9 +17,14 @@ def main():
         colorList += [ord(i)//16,ord(i)%16]
         #print("Decimal value: ", ord(i))
         #print("Hex value: ", ord(i)//16, ord(i)%16)
+    print(len(colorList))
+    for i in range(0, (len(colorList)//2)+1, 2):
+        print(colorList[i:i+2], end = "")
+    print(" ")
     for i in range(len(colorList)):
         print(colorList[i], end = "")
     print(" ")
-    colorTurtle()
+    sys = lsystem.Lsystem("Rules1.txt")
+    #colorTurtle()
 
 main()
