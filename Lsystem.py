228aa31b6570f4218ae52e1b8f202c99b2d3124a
Dyandra Allen <dallen9@binghamton.edu>
2019-02-09 16:26:25 -0500

    Lsystem file

---
+++ Lsystem.py
@@ -0,0 +1,12 @@
+class Lsystem:
+    def __init__(self,filename):
+        pass
+
+    def createLsystem(self):
+        pass
+
+    def applyRules(self,string):
+        pass
+
+    def drawLSystem(self, snap):
+        pass
--- color.py
+++ color.py
@@ -1,10 +1,19 @@
 import turtle
 
+def colorTurtle():
+    wn = turtle.Screen()
+    wn.exitonclick()
+
 def main():
-    word = input("Please enter a word: ")
+    word = input("Please enter a three letter word: ")
+    colorList = []
     for i in word:
-        c = char(word[i])
-    for c in range(len(word)):
-        
+        colorList += [ord(i)//16,ord(i)%16]
+        #print("Decimal value: ", ord(i))
+        #print("Hex value: ", ord(i)//16, ord(i)%16)
+    for i in range(len(colorList)):
+        print(colorList[i], end = "")
+    print(" ")
+    colorTurtle()
 
 main()
