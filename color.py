c677acd45d61e3dbf3ffffbeb1cca70dfd2cbe92
Dyandra Allen <dallen9@binghamton.edu>
2019-02-10 12:21:43 -0500

    Final

--- color.py
+++ color.py
@@ -3,8 +3,21 @@ import random
 import lsystem
 
 """
+    Randomizes the lsystem and sets the turtle position
+    args: snap(turtle)
+    return: fn (text file)
+"""
+def randLsystem(snap):
+    fn = "Rules" + str(random.randint(2,3)) + ".txt"
+    if(fn == "Rules3.txt"):
+        snap.setpos(-325,325)
+    else:
+        snap.setpos(0,0)
+    return fn
+
+"""
     Creates the turtle (Jeff) and screen
-    args: color (tuple of ints)
+    args: colors (list of tuples of ints)
     return: None
 """
 def colorTurtle(colors):
@@ -16,12 +29,10 @@ def colorTurtle(colors):
     jeff.shape("turtle")
     jeff.pensize(5)
     jeff.penup()
-    jeff.setpos(-325,325)
+    #jeff.setpos(-325,325)
     jeff.pendown()
     jeff.speed(0)
-
-    sys = lsystem.Lsystem("Rules3.txt")
-    #jeff.pencolor(colors[i])
+    sys = lsystem.Lsystem(randLsystem(jeff))
     sys.createLsystem()
     sys.drawLSystem(jeff,colors)
     wn.exitonclick()
@@ -30,7 +41,7 @@ def main():
     ''' Takes words from the user and changes them to the hex value - Adds 125 to make it lighter'''
     colorList = []
     while (len(colorList) < 9):
-        word = input("Please enter a three letter word: ")
+        word = input("Please enter a different 3-letter word: ")
         if len(word) > 3:
             print("You idiot, I said 3-letter word")
         elif len(word) == 0:
@@ -38,8 +49,8 @@ def main():
         else:
             for char in word:
                 colorList += [(ord(char)//16) * 10 + (ord(char)%16 + 125)]
-    #print(colorList)
-    #print(len(colorList))
+
+    ''' Adjusts colors if too high or low'''
     rgbList = []
     for i in range(0, len(colorList), 3):
         (r,g,b) = (colorList[i], colorList[i+1], colorList[i+2])
@@ -53,7 +64,7 @@ def main():
             else:
                 b += 20
         rgbList += [(r,g,b)]
-    #print(rgbList)
+
     colorTurtle(rgbList)
 
 main()
